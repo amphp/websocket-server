2fca652f8a8622d26bd6c856dbd6299a7c9cf07a
Aaron Piotrowski <aaron@trowski.com>
2018-03-11 21:52:06 -0500

    Add autobahn tests

---
+++ test-autobahn/server.php
@@ -0,0 +1,50 @@
+<?php
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+use Amp\Http\Server\Request;
+use Amp\Http\Server\Response;
+use Amp\Http\Server\Server;
+use Amp\Http\Server\Websocket;
+
+Amp\Loop::run(function () {
+    /* --- http://localhost:9001/ ------------------------------------------------------------------- */
+
+    $websocket = new Websocket\Websocket(new class implements Websocket\Application {
+        /** @var \Amp\Http\Server\Websocket\Endpoint */
+        private $endpoint;
+
+        public function onStart(Websocket\Endpoint $endpoint) {
+            $this->endpoint = $endpoint;
+        }
+
+        public function onHandshake(Request $request, Response $response) {
+            return $response;
+        }
+
+        public function onOpen(int $clientId, Request $request) { }
+
+        public function onData(int $clientId, Websocket\Message $message) {
+            if ($message->isBinary()) {
+                $this->endpoint->broadcastBinary(yield $message->buffer());
+            } else {
+                $this->endpoint->broadcast(yield $message->buffer());
+            }
+        }
+
+        public function onClose(int $clientId, int $code, string $reason) { }
+
+        public function onStop() { }
+    });
+
+    $websocket->setMaxBytesPerMinute(PHP_INT_MAX);
+    $websocket->setMaxFrameSize(PHP_INT_MAX);
+    $websocket->setMaxFramesPerSecond(PHP_INT_MAX);
+    $websocket->setMessageSizeLimit(PHP_INT_MAX);
+    $websocket->validateUtf8(true);
+
+    $server = new Server($websocket);
+    $server->expose("127.0.0.1", 9001);
+
+    return $server->start();
+});
