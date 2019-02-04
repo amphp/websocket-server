1d5351fcd0d660e424e6e4183ea438ca6fb2aa83
Aaron Piotrowski <aaron@trowski.com>
2019-02-03 23:48:33 -0600

    Finalize option mehods

--- src/Options.php
+++ src/Options.php
@@ -7,12 +7,12 @@ class Options extends \Amp\Websocket\Options
     private $heartbeatPeriod = 10;
     private $queuedPingLimit = 3;
 
-    public function getHeartbeatPeriod(): int
+    final public function getHeartbeatPeriod(): int
     {
         return $this->heartbeatPeriod;
     }
 
-    public function withHeartbeatPeriod(int $heartbeatPeriod): self
+    final public function withHeartbeatPeriod(int $heartbeatPeriod): self
     {
         if ($heartbeatPeriod < 1) {
             throw new \Error('$heartbeatPeriod must be a positive integer greater than 0');
@@ -24,12 +24,12 @@ class Options extends \Amp\Websocket\Options
         return $clone;
     }
 
-    public function getQueuedPingLimit(): int
+    final public function getQueuedPingLimit(): int
     {
         return $this->queuedPingLimit;
     }
 
-    public function withQueuedPingLimit(int $queuedPingLimit): self
+    final public function withQueuedPingLimit(int $queuedPingLimit): self
     {
         if ($queuedPingLimit < 1) {
             throw new \Error('$queuedPingLimit must be a positive integer greater than 0');
