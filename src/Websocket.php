ecfbaa511e9a42e28c25c1ec1c42e6b6b7604114
Charles Sprayberry <me@cspray.io>
2019-09-23 21:41:18 -0400

    more clarification on status codes

--- src/Websocket.php
+++ src/Websocket.php
@@ -61,11 +61,15 @@ abstract class Websocket implements RequestHandler, ServerObserver
 
     /**
      * Respond to websocket handshake requests.
+     *
      * If a websocket application doesn't wish to impose any special constraints on the
      * handshake it doesn't have to do anything in this method (other than return the
      * given Response object) and all handshakes will be automatically accepted.
-     * This method provides an opportunity to set application-specific headers and/or
-     * cookies on the websocket response.
+     *
+     * This method provides an opportunity to set application-specific headers, including
+     * cookies, on the websocket response. Although any non-101 status code can be used
+     * to reject the websocket connection it is generally recommended to use a 4xx status
+     * code that is descriptive of why the handshake was rejected.
      *
      * @param Request  $request The HTTP request that instigated the handshake
      * @param Response $response The switching protocol response for adding headers, etc.
