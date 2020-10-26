58934515ba5bfc9e79e912ec36b14636d119c4b0
Aaron Piotrowski <aaron@trowski.com>
2020-10-26 17:22:59 -0500

    Update autobahn test server

--- test-autobahn/server.php
+++ test-autobahn/server.php
@@ -5,9 +5,7 @@ require \dirname(__DIR__) . "/vendor/autoload.php";
 use Amp\Http\Server\HttpServer;
 use Amp\Http\Server\Request;
 use Amp\Http\Server\Response;
-use Amp\Promise;
 use Amp\Socket\Server;
-use Amp\Success;
 use Amp\Websocket\Client;
 use Amp\Websocket\Message;
 use Amp\Websocket\Options;
@@ -15,38 +13,40 @@ use Amp\Websocket\Server\ClientHandler;
 use Amp\Websocket\Server\Gateway;
 use Amp\Websocket\Server\Websocket;
 use Psr\Log\NullLogger;
+use function Amp\await;
 
-Amp\Loop::run(function (): Promise {
-    /* --- http://localhost:9001/ ------------------------------------------------------------------- */
+/* --- http://localhost:9001/ ------------------------------------------------------------------- */
 
-    $options = Options::createServerDefault()
-        ->withBytesPerSecondLimit(\PHP_INT_MAX)
-        ->withFrameSizeLimit(\PHP_INT_MAX)
-        ->withFramesPerSecondLimit(\PHP_INT_MAX)
-        ->withMessageSizeLimit(\PHP_INT_MAX)
-        ->withValidateUtf8(true);
+$options = Options::createServerDefault()
+    ->withBytesPerSecondLimit(\PHP_INT_MAX)
+    ->withFrameSizeLimit(\PHP_INT_MAX)
+    ->withFramesPerSecondLimit(\PHP_INT_MAX)
+    ->withMessageSizeLimit(\PHP_INT_MAX)
+    ->withValidateUtf8(true);
 
-    $websocket = new Websocket(new class implements ClientHandler {
-        public function handleHandshake(Gateway $gateway, Request $request, Response $response): Promise
-        {
-            return new Success($response);
+$websocket = new Websocket(new class implements ClientHandler {
+    public function handleHandshake(Gateway $gateway, Request $request, Response $response): Response
+    {
+        return $response;
+    }
+
+    public function handleClient(Gateway $gateway, Client $client, Request $request, Response $response): void
+    {
+        while ($message = $client->receive()) {
+            \assert($message instanceof Message);
+            if ($message->isBinary()) {
+                await($gateway->broadcastBinary($message->buffer()));
+            } else {
+                await($gateway->broadcast($message->buffer()));
+            }
         }
+    }
+}, $options);
 
-        public function handleClient(Gateway $gateway, Client $client, Request $request, Response $response): Promise
-        {
-            return Amp\call(function () use ($gateway, $client) {
-                while ($message = yield $client->receive()) {
-                    \assert($message instanceof Message);
-                    if ($message->isBinary()) {
-                        yield $gateway->broadcastBinary(yield $message->buffer());
-                    } else {
-                        yield $gateway->broadcast(yield $message->buffer());
-                    }
-                }
-            });
-        }
-    }, $options);
+$server = new HttpServer([Server::listen("127.0.0.1:9001")], $websocket, new NullLogger);
 
-    $server = new HttpServer([Server::listen("127.0.0.1:9001")], $websocket, new NullLogger);
-    return $server->start();
-});
+$server->start();
+
+$signal = Amp\signal(\SIGINT, \SIGTERM, \SIGSTOP);
+
+$server->stop();
