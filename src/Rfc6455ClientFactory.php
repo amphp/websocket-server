0597a813c8b554fe9cd09e111b75bb4f6f83ef72
Alexander Pankratov <s.pankratoff@gmail.com>
2023-01-30 01:58:58 +0100

    Fix: use namespaces

--- src/Rfc6455ClientFactory.php
+++ src/Rfc6455ClientFactory.php
@@ -5,12 +5,10 @@ namespace Amp\Websocket\Server;
 use Amp\Http\Server\Request;
 use Amp\Http\Server\Response;
 use Amp\Socket\Socket;
-use Amp\Websocket\Compression\CompressionContext;
+use Amp\Websocket\CompressionContext;
 use Amp\Websocket\DefaultHeartbeatQueue;
 use Amp\Websocket\DefaultRateLimiter;
 use Amp\Websocket\HeartbeatQueue;
-use Amp\Websocket\Parser\Rfc6455ParserFactory;
-use Amp\Websocket\Parser\WebsocketParserFactory;
 use Amp\Websocket\RateLimiter;
 use Amp\Websocket\Rfc6455Client;
 use Amp\Websocket\WebsocketClient;
@@ -20,7 +18,6 @@ final class Rfc6455ClientFactory implements WebsocketClientFactory
     public function __construct(
         private readonly ?HeartbeatQueue $heartbeatQueue = new DefaultHeartbeatQueue(),
         private readonly ?RateLimiter $rateLimiter = new DefaultRateLimiter(),
-        private readonly WebsocketParserFactory $parserFactory = new Rfc6455ParserFactory(),
         private readonly int $frameSplitThreshold = Rfc6455Client::DEFAULT_FRAME_SPLIT_THRESHOLD,
         private readonly float $closePeriod = Rfc6455Client::DEFAULT_CLOSE_PERIOD,
     ) {
@@ -35,7 +32,6 @@ final class Rfc6455ClientFactory implements WebsocketClientFactory
         return new Rfc6455Client(
             socket: $socket,
             masked: false,
-            parserFactory: $this->parserFactory,
             compressionContext: $compressionContext,
             heartbeatQueue: $this->heartbeatQueue,
             rateLimiter: $this->rateLimiter,
