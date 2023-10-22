f5bb36be1513a4f2f0e34a40ae90711a8cb4532a
Aaron Piotrowski <aaron@trowski.com>
2023-10-22 09:19:45 -0500

    Add simple body to upgrade error responses

--- src/Internal/UpgradeErrorHandler.php
+++ src/Internal/UpgradeErrorHandler.php
@@ -4,6 +4,7 @@ namespace Amp\Websocket\Server\Internal;
 
 use Amp\ForbidCloning;
 use Amp\ForbidSerialization;
+use Amp\Http\HttpStatus;
 use Amp\Http\Server\ErrorHandler;
 use Amp\Http\Server\Request;
 use Amp\Http\Server\Response;
@@ -16,6 +17,10 @@ final class UpgradeErrorHandler implements ErrorHandler
 
     public function handleError(int $status, ?string $reason = null, ?Request $request = null): Response
     {
-        return new Response($status, ['connection' => 'close']);
+        return new Response(
+            status: $status,
+            headers: ['content-type' => 'text/plain; charset=utf-8', 'connection' => 'close'],
+            body: \sprintf('%d %s', $status, $reason ?? HttpStatus::getReason($status)),
+        );
     }
 }
